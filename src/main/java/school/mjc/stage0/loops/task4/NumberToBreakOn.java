04e15d0dab7f600cbeb88a167486d65e10da44e6
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-08 12:17:45 +0500

    ""

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task4;
 
+import java.util.Scanner;
+
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        for (int i = 1; i<numberToGoUntil; i++) {
 
+            if (i > toBreakWith) {
+                break;
+
+            }
+            if (toBreakWith > numberToGoUntil){
+                System.out.print(i);
+            }else {
+
+            System.out.print( i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        NumberToBreakOn ss = new NumberToBreakOn();
+        ss.printNumbersUntilWithBreakOn(a,b);
     }
 }
